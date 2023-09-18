884bf974c43ef2ca57d5a8a6905c2b619a7fd383
Ost <vojvol@post.cz>
2023-09-18 16:07:00 +0200

    Splited to functions, Added ideal_test (written and saved image on computer) Added progress Started working on textProcessing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,55 @@
 <script lang="ts">
-	import Tesseract from 'tesseract.js';
-	Tesseract.recognize('https://cdn.discordapp.com/attachments/769169118980734996/1153043192065831043/20230917_200524.jpg', 'ces', {
-		logger: (m) => console.log(m)
+    // Tesseract
+    import Tesseract from 'tesseract.js';
+
+
+    let readingProgress = 0;
+    let processedData;
+
+    // OCR image
+    function readImage(imgURL: string) {
+	Tesseract.recognize(imgURL, 'ces', {
+		logger: (m) => readingProgress = m.progress*100
 	}).then(({ data: { text } }) => {
-		console.log(text);
-	});
+		processedData = processData(text)
+	});}
+    // Načtení obrázku
+    function loadFile(event) {
+	    const image: HTMLImageElement = document.getElementById('output');
+	    image.src = URL.createObjectURL(event.target.files[0]);
+        readImage(image?.src)
+    };
+    // Zpracování textu
+    function processData(rawReadData: string){
+        let sentences = rawReadData.split("\n")
+        //TODO regex, co nejdříve pozná otázku "####. text :""
+        //TODO regex, co oddělí písmeno od odpovědi "x)
+        //TODO podle kapitalizace x) určí správnou/špatnou odpověď
+        //TODO vytvoří objekt, kde bude Otázka, 4 odpovědi (i s písmenem, která budou všechny malá), true/false v závislosti na správnosti odpovědi
+        //console.log(sentences)
+        /**
+         * 454. Zásobní látkou hub je:
+         * a) škrob a glykogen
+         * B) glykogen a olej
+         * c) olej a škrob
+         * d) škrob, olej a glykoge
+         * 455. Spojováním buněk hub (Fungi) vznikají:
+         * A) nepravá pletiva
+         * B) hyfy
+         * C) mycelium
+         * D) pseudoparenchym
+        */
+    }
 </script>
 
 <header />
 
-<main />
+<main>
+    <p><input type="file"  accept="image/*" name="image" id="file"  on:change={() => loadFile(event)} style="display: none;"></p>
+<p><label for="file" style="cursor: pointer;">Upload Image</label></p>
+<p><img id="output" width="200" /></p> 
+<progress id="readingFile" value={readingProgress} max="100">{readingProgress}%</progress>
+</main>
 <footer>
 	Vytvořil <a href="https://github.com/ostSTRUPpen">ostSTRUPpen</a>
 </footer>
